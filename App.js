91267b14e0e5697a95cf6af980117d3526e9f9e9
faris110900 <faris.riskilail@gmail.com>
2020-07-29 21:39:58 +0700

    second

--- App.js
+++ App.js
@@ -1,114 +1,117 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+  Text, 
+  View, 
+  Image, 
+  TextInput,
+  StyleSheet
 } from 'react-native';
+import laptop from './laptop.jpg';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Hello World</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+    <StylingReactNativeComponent />
+  );
+};
+
+const StylingReactNativeComponent = () => {
+  return (
+    <View>
+      {/* <Text style={styles.text}>Styling Component</Text>
+      <View style={{
+        width: 100,
+        height: 100,
+        backgroundColor: 'blue',
+        borderWidth: 2,
+        borderColor: 'red',
+        marginTop: 30,
+        marginLeft: 30,
+      }} /> */}
+      <View style={{padding: 12, 
+        backgroundColor: '#f2f2f2', 
+        width: 212, 
+        borderRadius: 8,
+        marginLeft: 30,
+        marginTop: 30
+        }}>
+        <Image source={laptop} style={{
+          width: 188, 
+          height: 107, 
+          borderRadius: 8
+          }} />
+          <Text style={{
+            fontSize: 14, 
+            fontWeight: 'bold', 
+            marginTop: 16}}>New Mackbook 2020</Text>
+          <Text style={{
+            fontSize: 12,
+            color: '#f2994a',
+            fontWeight: 'bold',
+            marginTop: 12
+          }}>Rp.25.000.000</Text>
+          <Text style={{
+            fontSize: 12,
+            fontWeight: '300',
+            marginTop: 12
+          }}>Jakarta Barat</Text>
+          <View style={{
+            backgroundColor: '#6fcf97',
+            paddingVertical: 6,
+            borderRadius: 25,
+            marginTop: 20,
+          }}>
+            <Text style={{
+              fontSize: 14, 
+              fontWeight: '600', 
+              color: 'white',
+              textAlign: 'center',
+            }}>Beli</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
+  text: {
     fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    fontWeight: 'bold',
+    color: 'green',
+    marginLeft: 20,
+  }
 });
+
+const SampleComponent = () => {
+  return (
+    <View>
+    <View style={{width: 80, height: 80, backgroundColor: 'green'}} />
+    <Text>Hello World</Text>
+    <Text><Sambung/> belajar react native</Text>
+    <Photo />
+    <TextInput style={{borderWidth: 1}} />
+    <Profile />
+  </View>
+  );
+};
+
+const Sambung = () => {
+  return <Text>Saya</Text>
+};
+
+
+const Photo = () => {
+  return <Image source={{uri: 'https://placeimg.com/100/100/animals'}} style={{width: 100, height: 100}}/>
+};
+
+class Profile extends Component {
+  render() {
+    return (
+      <View>
+        <Image source={{uri: 'https://placeimg.com/100/100/nature'}} style={{width: 100, height: 100, borderRadius: 50}} />
+        <Text>Ini Foto Profile</Text>
+      </View>
+    );
+  }
+}
 
 export default App;
